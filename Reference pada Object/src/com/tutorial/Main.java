07bc466b29c9af03c3340e05ea90236099c738a9
Pedro'Korwa <59316805+Pedro-korwa@users.noreply.github.com>
2021-03-07 23:31:30 +0700

    01:31

--- Reference pada Object/src/com/tutorial/Main.java
+++ Reference pada Object/src/com/tutorial/Main.java
@@ -26,3 +26,30 @@ public class Main {
         System.out.println(Buku1);
     }
 }
+
+
+
+// Bagian Ke Dua
+   class Daftar {
+      /*ini adalah modifier*/public /* ini adalah member*/  String Lagu;
+       private String musisi;
+
+       Daftar(String Lagu , String musisi){
+           this.Lagu = Lagu;
+           this.musisi = musisi;
+
+  }
+      void show(){
+          System.out.println("\nJudul Lagu : " + this.Lagu);
+          System.out.println("Nama Musisi : " + this.musisi);
+      }
+
+   }
+
+   public class Main {
+       public static void main (String[] args){
+           Daftar Bagian = new Daftar("To The Bone " , "Pamugkas");
+           System.out.println(Bagian.Lagu;
+
+       }
+}
