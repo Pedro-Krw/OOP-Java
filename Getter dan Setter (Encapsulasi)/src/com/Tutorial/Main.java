0b037386cc85efcc8fa522df41533ee54fa6ceae
pedrokorwa <korwapedro@gmail.com>
2021-03-08 22:55:09 +0700

    00:55

--- Getter dan Setter (Encapsulasi)/src/com/Tutorial/Main.java
+++ Getter dan Setter (Encapsulasi)/src/com/Tutorial/Main.java
@@ -3,14 +3,20 @@ package com.Tutorial;
    class Data{
        public int intPublic;
        private int intPrivate;
+       private  double doublePrivate;
 
        Data(){
            this.intPublic = 0;
            this.intPrivate = 0;
        }
-
+// ini bagian GETTER
        public int getIntPrivate() {
            return this.intPrivate;
+       }
+
+//       Ini bagian SETTER
+       void setIntPrivate(double value){
+          this.doublePrivate = value;
        }
    }
 
@@ -19,8 +25,11 @@ package com.Tutorial;
        Data Data = new Data();
           Data.intPublic = 10;
           System.out.println(Data.intPublic);
+//          Ini adalah GETTER Read Only Pada mudule Private
           int agnka = Data.getIntPrivate();
           System.out.println(agnka);
+//          Ini adalah SETTER Write only Pada Module Private
+          Data.setIntPrivate(0.3);
 
       }
 }
