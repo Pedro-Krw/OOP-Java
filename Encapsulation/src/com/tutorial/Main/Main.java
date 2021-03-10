4090180319346ea143f190dda67ebb3edbb4bb6b
pedrokorwa <korwapedro@gmail.com>
2021-03-10 22:34:07 +0700

    00:34

--- Encapsulation/src/com/tutorial/Main/Main.java
+++ Encapsulation/src/com/tutorial/Main/Main.java
@@ -13,15 +13,36 @@ class Player{
 
     public  void show (){
         System.out.println("Nama  : " + this.nama);
-        System.out.println("Darah : " + this.darah);
+        System.out.println("Darah : " + this.MaxHeatht());
         System.out.println("Armor : "+ this.armor);
     }
 
+   public int MaxHeatht(){
+        return this.darah + this.armor * 20;
+   }
+
 }
+
+class armor{
+    private String name;
+    private int senjata;
+
+    armor(String name , int senjata){
+        this.name = name;
+        this.senjata = senjata;
+    }
+  public int getSenjata(){
+        return this.senjata;
+  }
+
+}
+
+
  public class Main {
      public static void  main(String[] args){
          Player Player = new Player("Martin" );
          Player.nama = "Budi";
+         armor armor = new armor("Daran Putih" , 200);
          Player.show();
 
      }
