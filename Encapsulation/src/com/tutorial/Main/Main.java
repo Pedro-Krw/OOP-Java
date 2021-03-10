53d6c82f68235021b9e449fc391691af16a75371
pedrokorwa <korwapedro@gmail.com>
2021-03-10 21:06:59 +0700

    23:06

--- Encapsulation/src/com/tutorial/Main/Main.java
+++ Encapsulation/src/com/tutorial/Main/Main.java
@@ -2,7 +2,7 @@ package com.tutorial.Main;
 
 public class Main {
       public static void main(String []args){
-          System.out.println("jalankan perintah saya");
+          System.out.println("jalankan perintah saya Sekarang juga");
       }
 
 }
