b0300b80ffa022ca9a66e566ccb3b53488c4fb40
pedrokorwa <korwapedro@gmail.com>
2021-03-10 21:26:54 +0700

    23:26

--- Encapsulation/src/com/tutorial/Main/Main.java
+++ Encapsulation/src/com/tutorial/Main/Main.java
@@ -1,8 +1,27 @@
 package com.tutorial.Main;
 
-public class Main {
-      public static void main(String []args){
-          System.out.println("jalankan perintah saya Sekarang juga");
-      }
+class Player{
+    String nama;
+    private int darah;
+    private  int armor;
 
+    Player(String nama){
+        this.nama = nama;
+        this.darah = 100;
+        this.armor = 100;
+    }
+
+    public  void show (){
+        System.out.println("Nama  : " + this.nama);
+        System.out.println("Darah : " + this.darah);
+        System.out.println("Armor : "+ this.armor);
+    }
+
+}
+ public class Main {
+     public static void  main(String[] args){
+         Player Player = new Player("Martin" );
+         Player.show();
+
+     }
 }
