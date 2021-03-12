89aeabb6a74620e2478f2234844bdace1b385733
pedrokorwa <korwapedro@gmail.com>
2021-03-12 21:21:16 +0700

    21:21

--- Encapsulation/src/com/tutorial/Main/Main.java
+++ Encapsulation/src/com/tutorial/Main/Main.java
@@ -1,49 +1,23 @@
-package com.tutorial.Main;
+package com.tutorial;
 
-class Player{
-    public String nama;
-    private int darah;
-    private  int armor;
+ class Player{
+     private String Name;
+     private Integer Darah;
+     private Integer Attack;
 
-    Player(String nama){
-        this.nama = nama;
-        this.darah = 100;
-        this.armor = 100;
-    }
+     Player()
 
-    public  void show (){
-        System.out.println("Nama  : " + this.nama);
-        System.out.println("Darah : " + this.MaxHeatht());
-        System.out.println("Armor : "+ this.armor);
-    }
-
-   public int MaxHeatht(){
-        return this.darah + this.armor * 20;
-   }
-
-}
-
-class armor{
-    private String name;
-    private int senjata;
-
-    armor(String name , int senjata){
-        this.name = name;
-        this.senjata = senjata;
-    }
-  public int getSenjata(){
-        return this.senjata;
-  }
-
-}
+ }
 
 
  public class Main {
-     public static void  main(String[] args){
-         Player Player = new Player("Martin" );
-         Player.nama = "Budi";
-         armor armor = new armor("Daran Putih" , 200);
-         Player.show();
+     public static void main(String[]args){
+
+
+
+
+
 
      }
+
 }
