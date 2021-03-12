b2e7f84ba2bd673d94e9b472d760e14e6b9cbf7b
pedrokorwa <korwapedro@gmail.com>
2021-03-12 22:19:30 +0700

    12:19

--- Encapsulation/src/com/tutorial/Main/Main.java
+++ Encapsulation/src/com/tutorial/Main/Main.java
@@ -9,15 +9,17 @@ package com.tutorial.Main;
            this.name = name;
            this.Speed = Speed;
        }
-
+     void show(){
+         System.out.println("Nama : " + this.name);
+         System.out.println("Attack : " +this.atrtack);
+         System.out.println("Speed : " + this.Speed);
+     }
 
    }
 
-
-
-
    public class Main {
-       public static void main{
-
+       public static void main(String[]args) {
+       player player = new player("Ica" , 20 , 100);
+       player.show();
        }
-}
+       }
