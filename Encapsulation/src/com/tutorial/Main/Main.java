04c1b6ddc6b7a7c78574b7778c946d8f724b02d0
pedrokorwa <korwapedro@gmail.com>
2021-03-13 17:55:35 +0700

    19:55

--- Encapsulation/src/com/tutorial/Main/Main.java
+++ Encapsulation/src/com/tutorial/Main/Main.java
@@ -3,22 +3,41 @@ package com.tutorial.Main;
      private String nama;
      private Integer power;
      private Integer Health;
+     private Armor armor;
 
      Player(String nama , Integer power , Integer Health){
          this.nama = nama;
          this.power = power;
          this.Health = Health;
      }
-     void show(){
-         System.out.println("Nama : " + this.nama );
+   public void show(){
+         System.out.println("Nama     : " + this.nama );
          System.out.println("Kekuatan : " + this.power);
-         System.out.println("Darah : " + this.Health);
+         System.out.println("Darah    : " + this.Health);
      }
+
+     public void setArmor(Armor armor) {
+         this.armor = armor;
+     }
+
  }
+
+class Armor{
+    private String armor;
+    private Integer kekuatan;
+
+    Armor(String armor , Integer kekuatan){
+        this.armor = armor;
+        this.kekuatan = kekuatan;
+    }
+
+}
 
  public class Main {
      public static void main(String [] args){
-    Player Player = new Player("MArtin" ,200 , 100);
+    Player Player = new Player("Martin" ,200 , 100);
     Player.show();
+    Armor armor1 = new Armor("Baju emas" , 200);
+    Player.setArmor(armor1);
      }
 }
