f38985a57a491fc0213c6a71f062e4a93e776814
pedrokorwa <korwapedro@gmail.com>
2021-03-13 19:11:33 +0700

    fix error

--- Encapsulation/src/com/tutorial/Main/Main.java
+++ Encapsulation/src/com/tutorial/Main/Main.java
@@ -3,7 +3,8 @@ package com.tutorial.Main;
      private String nama;
      private Integer power;
      private Integer Health;
-     private Armor armor;
+     private Armor armor1;
+     private senjata Senjata_Rahasia;
 
      Player(String nama , Integer power , Integer Health){
          this.nama = nama;
@@ -13,31 +14,58 @@ package com.tutorial.Main;
    public void show(){
          System.out.println("Nama     : " + this.nama );
          System.out.println("Kekuatan : " + this.power);
-         System.out.println("Darah    : " + this.Health);
+         System.out.println("Darah    : " + this.maxHealth());
      }
 
+
+  public int maxHealth(){
+ return this.Health + this.armor1.getHealth();
+  }
+
      public void setArmor(Armor armor) {
-         this.armor = armor;
+         this.armor1 = armor;
      }
+
+     public void setSenjata_Rahasia(senjata senjata){
+         this.Senjata_Rahasia = senjata;
+     }
+
 
  }
 
 class Armor{
     private String armor;
     private Integer kekuatan;
+    private  Integer health;
 
-    Armor(String armor , Integer kekuatan){
+    Armor(String armor , Integer kekuatan , Integer Health){
         this.armor = armor;
         this.kekuatan = kekuatan;
+        this.health = Health;
     }
 
+    public int getHealth() {
+        return this.kekuatan * 10 + this.health;
+    }
+}
+
+class senjata{
+    private String nama;
+     private Integer kekuatan;
+
+     senjata(String nama, Integer kekuatan){
+         this.nama = nama;
+         this.kekuatan = kekuatan;
+     }
 }
 
  public class Main {
      public static void main(String [] args){
     Player Player = new Player("Martin" ,200 , 100);
     Player.show();
-    Armor armor1 = new Armor("Baju emas" , 200);
+    Armor armor1 = new Armor("Baju emas" , 200 , 500);
     Player.setArmor(armor1);
+    senjata senjata1 = new senjata("Basoka" , 5000);
+    Player.setSenjata_Rahasia(senjata1);
      }
 }
