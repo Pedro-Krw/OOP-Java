a88cb98fa037c3b03fdc4d7b0abe38100b8e1d4c
pedrokorwa <korwapedro@gmail.com>
2021-03-14 22:06:35 +0700

    masih ada error

--- Encapsulation/src/com/tutorial/Main/Main.java
+++ Encapsulation/src/com/tutorial/Main/Main.java
@@ -1,54 +1,54 @@
 package com.tutorial.Main;
 
- class Player{
-     public String name;
-     private Integer Health;
-     private Integer Demage;
-     private Armor armor;
+class Player{
+    public String name;
+    private Integer Health;
+    private Integer Demage;
+    private Armor  armor;
 
-     Player(String name , Integer Health , Integer Demage){
-         this.name = name;
-         this.Health = Health;
-         this.Demage = Demage;
-     }
-      public void Show(){
-          System.out.println("Nama   : " + this.name);
-          System.out.println("Health : " + this.MaxHealth());
-          System.out.println("Demage : " + this.Demage);
-      }
-     public void setArmor(Armor armor) {
-         this.armor = armor;
-     }
+    Player(String name , Integer Health , Integer Demage ){
+        this.name = name;
+        this.Health = Health;
+        this.Demage = Demage;
+    }
+    public void Show(){
+        System.out.println("Nama   : " + this.name);
+        System.out.println("Health : " + this.MaxHealth());
+        System.out.println("Demage : " + this.Demage);
+        this.armor.getAddHealth();
+    }
+    public Armor setArmor(Armor armor) {
+        return this.armor = armor;
+    }
 
-     public int MaxHealth(){
-         return this.Health + this.armor.getAddHealth()*10;
-      }
+    public int MaxHealth(){
+        return this.Health + this.armor.getAddHealth()*10;
+    }
 
- }
+}
 
-  class Armor{
-     private String Name;
-     private  Integer Demage;
-     private Integer AddHealth;
+class Armor{
+    private String Name;
+    private  Integer Demage;
+    private Integer AddHealth;
 
     public Armor(String Name , Integer demage , Integer addHealth){
-         this.Name = Name;
-         this.Demage = demage;
-         this.AddHealth = addHealth;
-     }
+        this.Name = Name;
+        this.Demage = demage;
+        this.AddHealth = addHealth;
+    }
 
-      public Integer getAddHealth() {
-          return this.Demage *10 + this.AddHealth;
-      }
-  }
+    public Integer getAddHealth() {
+        return this.Demage *10 + this.AddHealth;
+    }
+}
 
 
 
- public class Main {
-     public static void main(String[]args){
-     Player player1 = new Player("Martin" , 100 , 2000);
-     Armor armor1 = new Armor("Baju Emas" , 2500 , 150);
-     player1.Show();
-     player1.setArmor(armor1);
-     }
+public class Main {
+    public static void main(String[]args){
+        Player player1 = new Player("Martin" , 100 , 2000  );
+        Armor armor = new Armor("Baju Emas" , 2500 , 150);
+        player1.Show();
+    }
 }
