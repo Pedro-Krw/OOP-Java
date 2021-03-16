a7d50c0e348e8909486209dc294659ce787f499b
pedrokorwa <korwapedro@gmail.com>
2021-03-16 21:13:21 +0700

    23:13

--- Encapsulation/src/com/tutorial/Main/Main.java
+++ Encapsulation/src/com/tutorial/Main/Main.java
@@ -4,6 +4,7 @@ package com.tutorial.Main;
       private String nama;
       private Integer Health;
       private Integer Weapon;
+      Armor armor;
 
       Player(String nama , Integer Health , Integer weapon){
           this.nama = nama;
@@ -18,13 +19,38 @@ package com.tutorial.Main;
           System.out.println("Weapon : " + this.Weapon);
       }
 
+     public void setArmor(Armor armor) {
+         this.armor = armor;
+     }
+
+     public int MaxHealth(){
+         return this.Health + this.armor.getKekuatan();
+     }
 
  }
+
+ class Armor {
+     private String nama;
+     private Integer Kekuatan;
+     private Integer trueDemage;
+
+     Armor (String nama , Integer kekuatan , Integer trueDemage){
+         this.nama = nama;
+         this.Kekuatan = kekuatan;
+         this.trueDemage = trueDemage;
+     }
+
+     public Integer getKekuatan() {
+         return this.Kekuatan + this.trueDemage;
+     }
+ }
+
 
 
  public class Main {
      public static void main(String[] args) {
          Player player1 = new Player("Martin" , 150 , 300);
+         Armor armor = new Armor("Parang" , 350 , 5000);
          player1.Show();
      }
 }
