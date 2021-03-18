e92649c42e9bf60ed6ffc670f68c0202974c0004
pedrokorwa <korwapedro@gmail.com>
2021-03-18 21:44:08 +0700

    Tambahan

--- Class Attributes/src/com/Tutorial/Main.java
+++ Class Attributes/src/com/Tutorial/Main.java
@@ -19,6 +19,8 @@ package com.Tutorial;
   public class Main {
       public static void main(String[] args) {
           Playing playing1 = new Playing("Martin");
+          Playing playing2 = new Playing("Joker");
+          playing2.show();
           playing1.show();
       }
 }
