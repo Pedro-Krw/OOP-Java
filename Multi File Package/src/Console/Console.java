61b027a73df3ef410134687ae551dc20a74561c7
pedrokorwa <korwapedro@gmail.com>
2021-03-19 13:20:33 +0700

    menambahakan library dari luar package

---
+++ Multi File Package/src/Console/Console.java
@@ -0,0 +1,10 @@
+package Console;
+
+public class Console {
+
+
+    public static void log(String nama){
+        System.out.println(nama);
+
+    }
+}
---
+++ Multi File Package/src/com/tutorial/Main.java
@@ -0,0 +1,27 @@
+package com.tutorial;
+import Console.Console;
+ class Player{
+     private String Orang;
+     private Integer jumlah;
+
+     Player(String nama , Integer jumlah){
+         this.Orang = nama;
+         this.jumlah = jumlah;
+     }
+
+     void show(){
+         System.out.println("nama   : " + this.Orang);
+         System.out.println("jumlah : " + this.jumlah );
+     }
+
+
+ }
+
+ public class Main {
+     public static void main(String[] args) {
+         Player player1 = new Player("Martin" , 20);
+         player1.show();
+         Console.log("Jamal");
+
+     }
+}
