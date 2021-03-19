04bf8421c6b969faed954efd71b4f2e1b309ba79
pedrokorwa <korwapedro@gmail.com>
2021-03-19 23:53:04 +0700

    Inheritance

--- Inheritance/src/com/tutorial/Main.java
+++ Inheritance/src/com/tutorial/Main.java
@@ -1,7 +1,23 @@
 package com.tutorial;
 
+ class Hero{
+     String nama;
+
+     Hero(){
+         this.nama = nama;
+     }
+ }
+
+ class Hero_tank extends Hero{
+ }
+
 public class Main {
     public static void main(String[] args) {
-
+        Hero hero1 = new Hero();
+        Hero hero2 = new Hero_tank();
+        hero1.nama = "Nama : Martin";
+        System.out.println(hero1.nama);
+        hero2.nama = "Nama : Kibax";
+        System.out.println(hero2.nama);
     }
 }
