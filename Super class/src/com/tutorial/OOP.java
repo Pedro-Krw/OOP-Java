5b57732b8bcca84a856ce3fcb11726ad595499ca
pedrokorwa <korwapedro@gmail.com>
2021-03-29 07:18:36 +0700

    new

--- Super class/src/com/tutorial/OOP.java
+++ Super class/src/com/tutorial/OOP.java
@@ -7,22 +7,23 @@ class Fungi{
 
     Fungi(String nama , String kata){
         this.nama = nama;
-        this.kata = kata;
+        this.kata = "makan";
     }
 
     void show(){
         System.out.println("Nama  :" + this.nama);
-        System.out.println("Nasi  :" + this.kata);
+        System.out.println(   this.kata);
     }
 
 }
 
 
 
-
 public class OOP {
-    public static void main (String[]args){
+    public static  void main(String[] args){
         Fungi fungi = new Fungi("Andi " , "Makan Nasi");
+        Fungi fungi1 = new Fungi("ana " , fungi.kata);
+        fungi1.show();
         fungi.show();
     }
 }
